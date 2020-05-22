8058c3193a4c626b5fe38b5888329dbc4f8ba137
Aawaj Joshi <s521315@nwmissouri.edu>
2020-05-22 02:16:56 -0500

    Add additional notes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
-import Header from './Header'
-import Footer from './Footer'
-import Note from './Note'
+import Header from './Header';
+import Footer from './Footer';
+import Note from './Note';
+import notes from '../notes';
 
 function App() {
     return (
         <div>
             <Header />
-            <Note />
+            {notes.map( notes => (
+                <Note 
+                    key = {notes.key}
+                    title = {notes.title}
+                    content = {notes.content}
+                />
+            ))}
             <Footer />
         </div>
     );
